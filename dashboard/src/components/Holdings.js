f89cc648559f006a83cbff7ed0a83c25d09da807
Soumya Tripathy <130821564+soumya007GitHub@users.noreply.github.com>
2026-05-02 12:20:49 +0530

    Update API endpoint for fetching all holdings

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -6,7 +6,7 @@ const Holdings = () => {
   const [allHoldings, setAllHoldings] = useState([]);
 
   useEffect(() => {
-    axios.get("http://18.60.153.239/allholdings").then((res) => {
+    axios.get("https://18.60.45.98/allholdings").then((res) => {
       setAllHoldings(res.data);
     });
   }, []);
